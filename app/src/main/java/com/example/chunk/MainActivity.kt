647093ee62db555f1f497bcab4ea054832b8f55b
maaaaaaaaad <integral5137@gmail.com>
2024-08-04 15:15:29 +0900

    e

--- app/src/main/java/com/example/chunk/MainActivity.kt
+++ app/src/main/java/com/example/chunk/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.chunk
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -16,18 +17,24 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PreViewMessageCard()
+            Column {
+                PreViewMessageCard()
+            }
         }
     }
 }
 
+data class Message(val one: String, val two: String, val three: String)
+
 @Composable
-fun MessageCard(message: String) {
-    Text(text = "Hello $message")
+fun MessageCard(message: Message) {
+    Text(text = message.one)
+    Text(text = message.two)
+    Text(text = message.three)
 }
 
 @Preview
 @Composable
 fun PreViewMessageCard() {
-    MessageCard(message = "Mad")
+    MessageCard(message = Message("1", "2", "3"))
 }
