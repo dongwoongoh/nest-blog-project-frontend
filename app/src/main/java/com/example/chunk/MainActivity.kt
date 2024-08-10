81984cefb0cd060499e6cb06b3213137a9187631
maaaaaaaaad <integral5137@gmail.com>
2024-08-10 10:44:03 +0900

    e

--- app/src/main/java/com/example/chunk/MainActivity.kt
+++ app/src/main/java/com/example/chunk/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -13,15 +15,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.chunk.ui.theme.ChunkTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Image(painter = painterResource(id = R.drawable.golang_char_2), contentDescription = "")
-            Column {
-                PreViewMessageCard()
+            Row(modifier = Modifier.padding(all = 8.dp)) {
+                Image(
+                    painter = painterResource(id = R.drawable.golang_char_2),
+                    contentDescription = ""
+                )
+                Column {
+                    PreViewMessageCard()
+                }
             }
         }
     }
